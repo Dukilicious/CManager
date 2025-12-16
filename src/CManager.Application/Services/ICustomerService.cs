37490fb5a480a5de7b3c60c369c31114d87e78b4
Duki <dukilicious@hotmail.com>
2025-12-16 19:13:58 +0100

    Added ICustomerService interface

---
+++ src/CManager.Application/Services/ICustomerService.cs
@@ -0,0 +1,9 @@
+using CManager.Domain.Models;
+
+namespace CManager.Application.Services;
+
+public interface ICustomerService
+{
+    bool CreateCustomer(string firstName, string lastName, string email, string phoneNumber, string streetAddress, string postalCode, string city);
+    IEnumerable<CustomerModel> GetAllCustomers(out bool hasError);
+}
