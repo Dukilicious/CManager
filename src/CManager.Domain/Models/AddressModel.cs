a3e3e67ca1dc018e8b83d63fa237a427655df2a2
Duki <dukilicious@hotmail.com>
2025-12-16 18:06:28 +0100

    Added AddressModel class

---
+++ src/CManager.Domain/Models/AddressModel.cs
@@ -0,0 +1,8 @@
+namespace CManager.Domain.Models;
+
+public class AddressModel
+{
+    public string StreetAdress { get; set; } = null!;
+    public string PostalCode { get; set; } = null!;
+    public string City { get; set; } = null!;
+}
